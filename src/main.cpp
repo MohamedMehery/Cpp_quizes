7dea46361ef30e89c7c7327123014ef9ec0b96b6
Mohamed Abdelnasser Mehery <es-mohamed.abdelnasser1415@alexu.edu.eg>
2022-08-18 21:42:26 +0200

    new assign style to lvalue and rvalue example

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,8 @@ using namespace std;
 
 int main (int argc , char * argv[])
 {
-    const int & lvalue = 10;
-    int && rvalue = 10;
-    int & lvalue = 2;   //error
+    const int & lvalue {10};
+    int && rvalue {10};
+    int & lvalue {2};   //error
     return 0;
 }
