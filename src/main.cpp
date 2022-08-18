37963e3b76ff9d92d0d6d8db552e14507aac05a5
Mohamed Abdelnasser Mehery <es-mohamed.abdelnasser1415@alexu.edu.eg>
2022-08-18 21:28:40 +0200

    Basic difference between lvalue and rvalue

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,8 @@ using namespace std;
 
 int main (int argc , char * argv[])
 {
-    while(argc--) 
-        std::cout << argv[argc] <<"\n";
-    std::cout<< std::endl;
+    const int & lvalue = 10;
+    int && rvalue = 10;
+    int & lvalue = 2;   //error
     return 0;
 }
