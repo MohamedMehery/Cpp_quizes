5fabc5979f01ecb0e496159f560540cd29c80920
MohamedMehery <49691904+MohamedMehery@users.noreply.github.com>
2023-12-24 18:24:31 +0200

    owning struct examble in basic cpp

--- src/basic_oop.cpp
+++ src/basic_oop.cpp
@@ -18,27 +18,86 @@ class Box
         Box(const Box & b) 
         {
             printf("Copy constructor\n");
+            this-> x_pos = b.x_pos;
+            this-> y_pos = b.y_pos;
         }
         void operator=(const Box & b)
         {
             printf("Assignment operator\n");
+            this-> x_pos = b.x_pos;
+            this-> y_pos = b.y_pos;
         }        
         void Call()
         {
-            printf("Call\n");
+            printf("x_pos = %d, y_pos = %d\n", x_pos, y_pos);
         }
-    private:
 
+        virtual Box & operator+(const Box & b){};
+
+    protected:
+        float x_pos{0}, y_pos{0};
 };
 
 class Carton : public Box
 {
     public:
         Carton() = default;     //Constructor must not be private
+
+        Carton & operator+(const Carton & c)
+        {
+
+
+            return *this;
+        }
+    public :
+        float x_pos, y_pos;
 };
+class Parent {
+protected:
+  void doSomething() {
+    // ... protected implementation
+    cout << "do something\n";
+    v++;
+  }
+
+  int v;
+};
+
+class Child : public Parent {
+public:
+  void doSomethingElse() {
+    this->doSomething(); // Calling parent's protected method directly
+    // ... additional logic
+    cout << v << endl;
+  }
+};
+
+
+struct OwningInt
+{
+    OwningInt() :  mint(new int)
+    {
+    }
+    
+    ~OwningInt()
+    {
+        delete mint;
+    }
+
+    private :
+        int * mint;
+};
+
+/** this function know that the integer has owning struct that control it's allocation deallocation in mem*/
+void owningfunc(const OwningInt & arg)
+{
+
+}
 
 int main (int argc , char * argv[])
 {
+    struct OwningInt;
+
     Dev_Error_t main_error = GLOBAL_ERROR;
     cout << main_error << endl;
     const int & lvalue {10};
@@ -51,6 +110,14 @@ int main (int argc , char * argv[])
     b3 = b1;        //Assignment operator will be called
     //Carton * c1 = &b1;    //error can't convert Box* to Carton*
     Carton c1;
+    c1.x_pos = 2;
+    c1.y_pos = 3;
+    Carton C2 = c1;
     Box * pb = &c1;
+
+    C2.Call();
+
+    Child ch1;
+    ch1.doSomethingElse();
     return 0;
 }
--- src/main.c
+++ src/main.c
@@ -1,20 +1,26 @@
+// 
 #include <stdio.h>
 
 typedef struct obj
 {
+
+    #pragma pack(4)
+    int x;
+
+    #pragma pack(8)
+    double d;
+
     // #pragma pack(1);
     // char c;    
-    #pragma pack(4);
-    int x;
-    #pragma pack(8);
-    double d;
-    #pragma pack(1);
+
+    #pragma pack(1)
     char c;
-}O;
+}O; __attribute__((PACKED))
 
 int main()
 {
-    puts("Hello");
+    printf("Hello %d\n" , 1<<16);
     printf("size of O %d\n" , sizeof(O));
     return 0;
 }
+
